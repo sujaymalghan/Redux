c5619f0a1e63689f579f1a4080c472ff02ca20fb
sujay <sujayvmalghan@gmail.com>
2023-08-12 01:04:04 -0400

    Commit

---
+++ src/store/slices/userSlice.js
@@ -0,0 +1,57 @@
+import {createSlice} from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
+import { adduser } from '../thunks/addUser';
+
+const usersSlice = createSlice
+(
+{
+    name:'users',
+    initialState: {
+        data:[],
+        isLoading:false,
+        error:null
+
+},
+
+extraReducers(builder)
+{
+
+    builder.addCase(fetchUsers.pending , (state,action) => {
+        state.isLoading = true;
+
+});
+
+    builder.addCase(fetchUsers.fulfilled , (state,action) => {
+    state.isLoading = false;
+    state.data = action.payload;
+
+});
+
+    builder.addCase(fetchUsers.rejected, (state,action) => {
+        state.isLoading = false;
+        state.error = action.error;
+} 
+);
+
+builder.addCase(adduser.pending,(state,action)=>{
+state.isLoading = true;
+
+});
+builder.addCase(adduser.fulfilled ,(state,action) =>{
+
+state.isLoading=false;
+state.data.push(action.payload);
+
+});
+builder.addCase(adduser.rejected, (state,action) =>{
+    state.isLoading=false;
+    state.error=action.error;
+
+});
+
+},
+
+});
+
+
+export const usersReducer = usersSlice.reducer;
